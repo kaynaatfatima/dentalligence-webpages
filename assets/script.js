62f9ff147e4c57f1e7c3c20fdaac16276b9d2413
kaynaatfatima <kaynaat.fatima@jbs.live>
2023-05-17 16:04:37 +0500

    responsive header and navbar. - added script in index - footer fixed

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,18 @@ navToggle.addEventListener("click", function () {
     navbar.classList.add("active");
   }
 });
+
+// For index nav bar
+const i_navbar = document.getElementById("index-navbar");
+const i_navToggle = document.getElementById("index-nav-toggle");
+
+// Add event listener to nav-toggle button
+i_navToggle.addEventListener("click", function () {
+  if (i_navbar.classList.contains("active")) {
+    // If active class is present, remove it
+    i_navbar.classList.remove("active");
+  } else {
+    // If active class is not present, add it
+    i_navbar.classList.add("active");
+  }
+});
